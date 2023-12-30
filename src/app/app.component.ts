b4f1038e2aa5260dbbfed524ed10050d8b5e33f8
swamimalavade <swami.91196@gmail.com>
2023-12-30 12:03:55 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { FirstComponentComponent } from './first-component/first-component.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SecondComponentComponent, FirstComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
